4eb0a5344fc4245544bf04f4986dcfb1c6dab495
sean wong <sean.wong.2009@business.smu.edu.sg>
2024-01-23 11:00:27 +0800

    testing

---
+++ Components/Welcome.tsx
@@ -0,0 +1,16 @@
+import { Text } from "react-native";
+
+interface WelcomeProps {
+  name: string;
+  age: number;
+  gender: boolean;
+}
+
+export default function Welcome({ name, age, gender }: WelcomeProps) {
+  return (
+    <Text>
+      Welcome to React-hell {name}. You are already {age}, time to start acting
+      your age. {"\n"}Gender: {gender ? "Male" : "Female"}
+    </Text>
+  );
+}
